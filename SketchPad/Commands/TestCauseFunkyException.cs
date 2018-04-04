7c2a8dacf0262b348650f16f4e23fd7c8e7b0fa0
Nathan Letwory <nathan@mcneel.com>
2018-04-04 10:33:31 +0300

    throw FunkyException when running command.

---
+++ SketchPad/Commands/TestCauseFunkyException.cs
@@ -0,0 +1,22 @@
+﻿using Rhino;
+using Rhino.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SketchPad.Commands
+{
+	public class FunkyException:Exception {}
+
+	public class TestCauseFunkyException : Command
+	{
+		public override string EnglishName => "TestCauseFunkyException";
+
+		protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+		{
+			throw new FunkyException();
+		}
+	}
+}
